6051736e5a5d6c7150934dc7cd29532c0027765c
Lance Finney <lance.finney@oasisdigital.com>
2023-11-10 11:12:53 -0600

    curriculum update

--- src/app/order-list/order-list.component.ts
+++ src/app/order-list/order-list.component.ts
@@ -1,10 +1,13 @@
+import { NgFor } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 import { Order } from '../api-types';
 
 @Component({
   selector: 'app-order-list',
-  templateUrl: './order-list.component.html'
+  templateUrl: './order-list.component.html',
+  standalone: true,
+  imports: [NgFor]
 })
 export class OrderListComponent {
   // defaults to property name = variable name.
